d5276a0a4dd1d23a9098ab251ad90121d1a771b3
Marden Rocha <mardenbiel@gmail.com>
2021-03-28 22:59:08 -0300

    Add a routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailComponent } from './components/email/email.component';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './helpers/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomeComponent, canActivate:[AuthGuard]},
+  {path:'login',component:LoginComponent},
+  {path:'form',component: FormComponent,canActivate:[AuthGuard]},
+  {path:'email',component: EmailComponent,canActivate:[AuthGuard]},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -28,14 +28,15 @@ const routes: Routes = [
     LoginComponent,
     FormComponent,
     EmailComponent,
- 
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule,
+    FormsModule
   ],
   exports:[
     RouterModule
